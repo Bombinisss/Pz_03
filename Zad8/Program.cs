270ae64a91e9835664022b7fd935c0387c440bba
Bombinisss <antekbom@gmail.com>
2022-11-06 11:23:24 +0100

    update zad8

--- Zad8/Program.cs
+++ Zad8/Program.cs
@@ -6,23 +6,38 @@ namespace Zad8
     {
         public static void Main(string[] args)
         {
-            int count = 0, result = 0;;
+            int result = 0,result2=0;
             for (int i = 1; i <= 500; i++)
             {
                 if (i % 2 != 0)
                 {
-                    Console.WriteLine(i);
                     result += i;
-                    count++;
+
+                    if (result % 7 != 0)
+                    {
+                        result -= i;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepodzielna przez 2: "+i);
+                        result2 += i;
+                    }
                 }
                 else if (i % 3 != 0)
                 {
-                    Console.WriteLine(i);
+                    
                     result += i;
-                    count++;
-                }
 
-                if (result % 7 != 0) result -= i;
+                    if (result % 7 != 0)
+                    {
+                        result -= i;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepodzielna przez 3: "+i);
+                        result2 += i;
+                    }
+                }
             }
             Console.WriteLine(result);
             Console.ReadKey();
