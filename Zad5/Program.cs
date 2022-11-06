1f3577e4ec6d2f77189136352a4a99c26367aec6
Bombinisss <antekbom@gmail.com>
2022-11-06 12:12:39 +0100

    update zad5

--- Zad5/Program.cs
+++ Zad5/Program.cs
@@ -4,16 +4,21 @@ namespace Zad5
 {
     internal class Program
     {
+        public enum PoryRoku
+        {
+            Wiosna, Lato, Jesień, Zima
+        }
         public static void Main(string[] args)
         {
-            string[] PoryRoku = { "Wiosna", "Lato", "Jesień", "Zima" };
-            string poraUlubiona = "Lato", poraNielubiana = "Jesień";
+            var c = (PoryRoku)2;
+            var d = (PoryRoku)3;
+            Console.WriteLine("Zgadnij jedno lubiane jako cyfra: Wiosna, Lato, Jesień, Zima");
+            if (int.Parse(Console.ReadLine()) == (int)c) Console.WriteLine("użytkownik zgadł");
+            else Console.WriteLine("użytkownik niezgadł");
 
-            Console.WriteLine("Zgadnij jedno lubiane: Wiosna, Lato, Jesień, Zima");
-            if (Console.ReadLine() == poraUlubiona) Console.WriteLine("użytkownik zgadł");
-
-            Console.WriteLine("Zgadnij jedno nielubiane: Wiosna, Lato, Jesień, Zima");
-            if (Console.ReadLine() == poraNielubiana) Console.WriteLine("użytkownik zgadł");
+            Console.WriteLine("Zgadnij jedno nielubiane jako cyfra: Wiosna, Lato, Jesień, Zima");
+            if (int.Parse(Console.ReadLine()) == (int)d) Console.WriteLine("użytkownik zgadł");
+            else Console.WriteLine("użytkownik niezgadł");
 
             Console.ReadKey();
         }
