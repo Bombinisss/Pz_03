1d5722c054bd536c0a71a39bf0ee0acb6a6f062a
Bombinisss <antekbom@gmail.com>
2022-11-06 08:32:22 +0100

    update Zad5

--- Zad5/Program.cs
+++ Zad5/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace Zad5
+﻿using System;
+
+namespace Zad5
 {
     internal class Program
     {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args)
+        {
+            string[] PoryRoku = { "Wiosna", "Lato", "Jesień", "Zima"};
+            string poraUlubiona = "Lato",poraNielubiana = "Jesień";
+            
+            Console.WriteLine("Zgadnij jedno lubiane: Wiosna, Lato, Jesień, Zima");
+            if (Console.ReadLine()==poraUlubiona){Console.WriteLine("użytkownik zgadł");}
+            
+            Console.WriteLine("Zgadnij jedno nielubiane: Wiosna, Lato, Jesień, Zima");
+            if (Console.ReadLine()==poraNielubiana){Console.WriteLine("użytkownik zgadł");}
+
+            Console.ReadKey();
+        }
     }
 }
